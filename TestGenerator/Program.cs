eb1485485a7057b3b4ca2b1e66c4187402d0927b
Ryan White <sunsetquest@hotmail.com>
2023-03-08 21:13:48 -0800

    added comment with error msg

--- TestGenerator/Program.cs
+++ TestGenerator/Program.cs
@@ -1,5 +1,5 @@
 ﻿using static System.Console;
-using SampleSourceGenerator;
+using SampleSourceGenerator; // <--- Error here: "The type or namespace name 'type/namespace' could not be found (are you missing a using directive or an assembly reference?)"
 
 WriteLine(ClassNames.Test);
 
